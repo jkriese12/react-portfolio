56e942f7605559b39e8ed669415e8070bde9b63e
Jacob Kriese <jkriese12@gmail.com>
2021-04-11 12:28:01 -0700

    Initial commit, boilerplate react app, bootstrap added

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>This is just the beginning</header>
+    </div>
+  );
+}
+
+export default App;
